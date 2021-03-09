3b0f54f8a05e8f84045f2a47c7fb0023b3995f24
pitoneggYolk <78996758+pitoneggYolk@users.noreply.github.com>
2021-03-09 08:33:08 +0300

    Update.DEV#4
    - Added atype where auth method

--- php/auth.php
+++ php/auth.php
@@ -3,16 +3,17 @@
 header("Access-Control-Allow-Origin: *");
 $login = $options['login'];
 $password = $options['password'];
+$stime = time();
 // Пытаемся получить подруг из базы
-if( ($query = dbquery("SELECT id, password FROM accounts WHERE login = '{$login}'")) != null ){
+if( ($query = dbquery("SELECT id, password, atype FROM accounts WHERE login = '{$login}'")) != null ){
     // Если пароль подходит, генерируем сессию
     if(password_verify($password, $query[0]['password'])){
         $id = $query[0]['id'];
         $skey = hash("sha256", $id.time());
         $ip = $_SERVER["REMOTE_ADDR"];
         // Если смог добавиться в базу, значит даём добро
-        if( dbexecute("INSERT INTO sessions (ip, aid, skey) VALUES ('{$ip}', '{$id}', '{$skey}')") ){
-            sendAnswer(true, ["session" => $skey]);
+        if( dbexecute("INSERT INTO sessions (ip, aid, skey, stime) VALUES ('{$ip}', '{$id}', '{$skey}', '{$stime})") ){
+            sendAnswer(true, ["session" => $skey, "atype" => $query['atype']]);
         }
     }
     sendAnswer(false, ["Неверный пароль"]);
